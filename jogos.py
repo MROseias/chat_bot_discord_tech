4353b5fd977721e40ef6207347d7de8b38d15033
Oséias Henrique <ozzcaires99@gmail.com>
2024-10-15 11:59:29 -0400

    pequenas alterações

--- jogos.py
+++ jogos.py
@@ -18,7 +18,7 @@ class Jogos(commands.Cog):
             else:
                 return None
 
-    async def buscar_jogos(self,jogos_que_achou):   
+    async def buscar_jogos(self,ctx,jogos_que_achou):   
         jogos="```"
         i=0
         for _, row in jogos_que_achou.iterrows():
@@ -29,10 +29,12 @@ class Jogos(commands.Cog):
                 jogos+=f"\nApp ID: {appid}, Nome do Jogo (PT-BR): {localized_name}"
                 print(f"\nApp ID: {appid}, Nome do Jogo (PT-BR): {localized_name}")
                 i=i+1
-                if(i==10):
-                    break
-        jogos+="\n```"
-        print(jogos)
+                if(i%10==0):
+                    jogos+="\n```"
+                    print(jogos)
+                    await ctx.send(jogos)
+                    jogos="```"
+                    
         return jogos    
         
     @commands.command(help="Esse comando serve para buscar o preço de um jogo na Steam.")
@@ -55,7 +57,7 @@ class Jogos(commands.Cog):
             if not matching_games.empty:
                 
                 await ctx.send("Preparando lista com os Ids dos jogos que eu encontrei: ")
-                await ctx.send(await self.buscar_jogos(matching_games))
+                await ctx.send(await self.buscar_jogos(ctx,matching_games))
             else:
                 await ctx.send("Não encontrei nenhum jogo :( . Você esqueceu de colocar algum espaço?")
 
@@ -100,7 +102,7 @@ class Jogos(commands.Cog):
                     nome=await self.get_localized_name(id)
                     dicionario={
                         "id":id,
-                        "nome":nome
+                        "nomeid":nome
                     }
                     dataframe=pd.DataFrame([dicionario])
                     dataframe.to_csv("favoritos.csv",mode="a", index=False, header=False)
